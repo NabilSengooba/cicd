fa502fa51980ed2e52f76a3d5a34f87f843964f6
NabilSengooba <nabilbil482@gmai.com>
2024-03-12 12:07:10 +0300

    intial setup

---
+++ bccs/src/main/java/cicd/com/bccs/BccsApplication.java
@@ -0,0 +1,13 @@
+package cicd.com.bccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BccsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BccsApplication.class, args);
+	}
+
+}
